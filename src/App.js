e2f5883ee8e457e258f56ef374d786b53fc037d6
Aaron <deas.aaron@gmail.com>
2021-03-29 02:38:10 -0400

    Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Login from "./Login";
+import Player from "./Player";
+import { getTokenFromUrl } from "./spotify";
 
 function App() {
+  const [token, setToken] = useState(null);
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+    if (_token) {
+      setToken(_token);
+    }
+    console.log("I HAVE A TOKEN>>> ", token);
+  }, []);
   return (
-    <div className="app">
-      <Login />
-    </div>
+    <div className="app">{token ? <h4>Hi I am logged in.</h4> : <Login />}</div>
   );
 }
 
